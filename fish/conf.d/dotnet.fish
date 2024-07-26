1759b09d647273fb1c46db7e0a7df7511da0747d
isaac <minimaleffort769@gmail.com>
2024-07-26 13:56:50 +0700

    impl(fish): support native package .NET root

--- fish/conf.d/dotnet.fish
+++ fish/conf.d/dotnet.fish
@@ -1,5 +1,10 @@
-if status is-interactive; and test -d "$HOME/.dotnet"
-  set -gx DOTNET_ROOT "$HOME/.dotnet"
+if status is-interactive
+  if test -x "$HOME/.dotnet/dotnet"
+    set -gx DOTNET_ROOT "$HOME/.dotnet"
+  else if test -x "/usr/share/dotnet/dotnet"
+    set -gx DOTNET_ROOT "/usr/share/dotnet"
+  end
+
   fish_add_path -gP "$DOTNET_ROOT"
-  fish_add_path -gP "$DOTNET_ROOT/tools"
+  fish_add_path -gP "$HOME/.dotnet/tools"
 end
