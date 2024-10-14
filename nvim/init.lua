4fd686cae978612e583898281554a3cb8de17a68
isaac <minimaleffort769@gmail.com>
2024-10-14 16:08:52 +0700

    feat(nvim): vertical finder view by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -215,6 +215,11 @@ require("lazy").setup({
     tag = "0.1.8",
     enabled = vim.g.vscode ~= 1,
     dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      defaults = {
+        layout_strategy = 'vertical'
+      },
+    },
     keys = {
       { "<leader>sf", "<cmd>Telescope find_files<cr>",             desc = "File browser" },
       { "<leader>sF", "<cmd>Telescope find_files hidden=true<cr>", desc = "File browser" },
